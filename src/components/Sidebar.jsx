e2e1055e28d071e789720a5703a6ab4897c19865
kabir <kabirehsan476@gamil.com>
2024-08-07 23:50:51 +0530

    complete the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,13 +4,9 @@ import "../css/sidebar.css";
 function Sidebar() {
   return (
     <div className="sidebar">
+      <div className="sidebar-title">Pocket Notes</div>
       <div className="sidebar-box">
-        <h2 className="sidebar-title">Pocket Notes</h2>
         <div className="sidebar-groups">
-          <div className="sidebar-group">
-            <div className="sidebar-profile">PW</div>
-            <p className="sidebar-group-text">PHYSICS WALLA</p>
-          </div>
           <div className="sidebar-group">
             <div className="sidebar-profile">PW</div>
             <p className="sidebar-group-text">PHYSICS WALLA</p>
